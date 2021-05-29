2adb91da12c9805fe6f9ebc2426ad44ccd99a1e8
Kajetan Kisielewski <kajetankisielewski@gmail.com>
2021-05-30 00:11:43 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 const btn = document.querySelector('.products__button');
-const form = document.querySelector('.form__container')
-const header = document.querySelector('header')
-const main = document.querySelector('main')
-const footer = document.querySelector('footer')
-const closer = document.querySelector('.form--close')
+const form = document.querySelector('.form__container');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+const closer = document.querySelector('.form--close');
+
+const infoData = document.querySelector('.info__data');
+const sendInput = document.querySelector('[type="submit"]');
+
+
 
 btn.addEventListener('click' , function() {
     header.classList.toggle('inactive')
     main.classList.toggle('inactive')
     footer.classList.toggle('inactive')
     form.classList.toggle('inactive')
+    window.scrollTo(0, 0);
 })
 
 closer.addEventListener('click' , function() {
@@ -17,8 +23,19 @@ closer.addEventListener('click' , function() {
     main.classList.toggle('inactive')
     footer.classList.toggle('inactive')
     form.classList.toggle('inactive')
+    window.scrollTo(0, 0);
+})
+
+
+sendInput.addEventListener('click' , function() {
+    infoData.style.visibility="visible";
 })
 
 
 
+const handleSubmit = function(e) {
+ e.preventDefault();
+}
+
+form.addEventListener('submit', handleSubmit);
 
